466a60bbe041508abd4666912fa0af32d2c6ec50
haze272 <studiorain27@gmail.com>
2022-11-11 21:02:48 +0300

    Маршрутизация

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/navigation/header/header.component';
 import { SidenavComponent } from './shared/components/navigation/sidenav/sidenav/sidenav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidenavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
