d33c08c1291406b86095d670b0ca14cf78881cae
haze272 <studiorain27@gmail.com>
2022-11-28 05:17:30 +0300

    Создание страницы редактирования задания

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let router = express.Router();
 const pool = require('../data/config');
+const {response} = require("express");
+
+taskRouter = express.Router({mergeParams: true});
 
 const requiredAuth = (req, res, next) => {
   if (res.locals.currentUser.isGuest()) {
@@ -13,14 +16,26 @@ const requiredAuth = (req, res, next) => {
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'FLIT - Управление сервисом' });
 });
+router.use('/tasks', taskRouter);
 
-router.get('/tasks', (request, response) => {
-  pool.query('SELECT * FROM tasks', (error, result) => {
+taskRouter.get('/', (req, res) => {
+  pool.query('SELECT * FROM tasks;', (error, result) => {
     if (error) throw error;
 
-    response.render('tasks', {
+    res.render('tasks', {
       title: 'Просмотр и редактирование заданий',
       tasks: result
+    });
+  });
+});
+
+taskRouter.get('/:task/edit', (req, res, next) => {
+  pool.query('SELECT * FROM tasks WHERE id=' + req.params["task"] + ';', (error, result) => {
+    if (error) throw error;
+
+    res.render('editTask', {
+      title: 'Редактирование задания',
+      task: result[0]
     });
   });
 });
