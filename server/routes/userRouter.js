c60b57948a98d1560d41e740a1b5d7a78eccd8cf
haze272 <studiorain27@gmail.com>
2022-12-15 16:34:58 +0300

    Отображение пользователей

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -5,24 +5,58 @@ const {response} = require("express");
 userRouter = express.Router({mergeParams: true});
 
 userRouter.get('/customers', (req, res) => {
-    pool.query('SELECT * FROM customers;', (error, result) => {
+    pool.query('SELECT\n' +
+        'customers.id,\n' +
+        'customers.name,\n' +
+        'customers.surname,\n' +
+        'customers.exp,\n' +
+        'customers.email,\n' +
+        'customers.password,\n' +
+        'customers.bio,\n' +
+        'ranks_customer.name as rank_name,\n' +
+        'ranks_customer.id as rank_id,\n' +
+        'customers.created_at,\n' +
+        'customers.updated_at,\n' +
+        'customers.avatar_url,\n' +
+        'customers.date_of_birth\n' +
+        'FROM customers\n' +
+        'JOIN ranks_customer ON\n' +
+        'customers.ranks_customer_id=ranks_customer.id\n' +
+        'GROUP BY customers.id;\n', (error, result) => {
         if (error) throw error;
 
-        res.render('list/customers', {
+        res.render('list/users', {
             title: 'Заказчики',
-            customers: result
+            users: result
         });
     });
 });
 
 userRouter.get('/performers', (req, res) => {
 
-    pool.query('SELECT * FROM performers;', (error, result) => {
+    pool.query('SELECT\n' +
+        'performers.id,\n' +
+        'performers.name,\n' +
+        'performers.surname,\n' +
+        'performers.exp,\n' +
+        'performers.email,\n' +
+        'performers.password,\n' +
+        'performers.bio,\n' +
+        'ranks_performer.name as rank_name,\n' +
+        'ranks_performer.id as ranks_id,\n' +
+        'performers.created_at,\n' +
+        'performers.updated_at,\n' +
+        'performers.avatar_url,\n' +
+        'performers.date_of_birth\n' +
+        'FROM performers\n' +
+        'JOIN ranks_performer ON\n' +
+        'performers.ranks_performer_id=ranks_performer.id\n' +
+        'GROUP BY performers.id;', (error, result) => {
         if (error) throw error;
 
-        res.render('list/performers', {
+        res.render('list/users', {
             title: 'Исполнители',
-            performers: result
+            users: result
         });
     });
 });
