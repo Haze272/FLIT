0043a074d0da676eafd5bd92234a631fedaaec1a
haze272 <studiorain27@gmail.com>
2022-12-19 04:36:00 +0300

    Редактирование рангов

--- server/routes/ranksRouter.js
+++ server/routes/ranksRouter.js
@@ -10,7 +10,8 @@ ranksRouter.get('/customers', (req, res) => {
 
         res.render('list/ranks', {
             title: 'Ранги заказчиков',
-            ranks: result
+            ranks: result,
+            type: 'customers'
         });
     });
 });
@@ -22,31 +23,56 @@ ranksRouter.get('/performers', (req, res) => {
 
         res.render('list/ranks', {
             title: 'Ранги исполнителей',
-            ranks: result
+            ranks: result,
+            type: 'performers'
         });
     });
 });
 
-ranksRouter.get('/:rank/edit', (req, res, next) => {
-    let taskTypes;
-    pool.query('SELECT * FROM task_type;', (error, result) => {
-        if (error) throw error;
-
-        taskTypes = result;
+ranksRouter.get('/:rank/edit', async (req, res, next) => {
+    let result;
+    if (req.query.type == 'customers') {ranks = await pool.promise().query('SELECT * FROM ranks_customer;').then( ([rows,fields]) => rows);
+        result = await pool.promise().query('SELECT * FROM ranks_customer WHERE id=' + req.params["rank"] + ';').then( ([rows,fields]) => rows);
+    } else if (req.query.type == 'performers') {
+        result = await pool.promise().query('SELECT * FROM ranks_performer WHERE id=' + req.params["rank"] + ';').then( ([rows,fields]) => rows);
+    }
+    res.render('edit/editRank', {
+        title: 'Редактирование ранга',
+        rank: result[0],
+        type: req.query.type
     });
+});
+ranksRouter.post('/:rank/edit', (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
 
+    let sql;
+    if (req.query.type == 'customers') {
+        sql = 'UPDATE ranks_customer SET\n' +
+            'name = \'' + req.body.name + '\',\n' +
+            'description = \'' + req.body.description + '\',\n' +
+            'exp_required = \'' + req.body.exp_required + '\'\n' +
+            'WHERE id=' + req.params["rank"] + ';';
+    } else if (req.query.type == 'performers') {
+        sql = 'UPDATE ranks_performer SET\n' +
+            'name = \'' + req.body.name + '\',\n' +
+            'description = \'' + req.body.description + '\',\n' +
+            'exp_required = \'' + req.body.exp_required + '\'\n' +
+            'WHERE id=' + req.params["rank"] + ';';
+    }
 
-    pool.query('SELECT * FROM tasks WHERE id=' + req.params["task"] + ';', (error, result) => {
-        if (error) throw error;
-
-
-        res.render('editTask', {
-            title: 'Редактирование задания',
-            task: result[0],
-            allTaskTypes: taskTypes,
-            currentTaskType: taskTypes[result[0].task_type_id]
-        });
-    });
+    pool.query(
+        sql,
+        (err) => {
+            if (err) console.log(err);
+            if (req.query.type == 'customers') {
+                res.redirect('/ranks/customers');
+            } else if (req.query.type == 'performers') {
+                res.redirect('/ranks/performers');
+            }
+        }
+    );
 });
 
 module.exports = ranksRouter;
